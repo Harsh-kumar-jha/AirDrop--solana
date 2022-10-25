6db8453d2e05cc1e4516a8461c3a509636ca0ae6
Harsh Kumar Jha <jhaharshkumar293@gmail.com>
2022-10-25 23:01:32 +0530

    created a function to get 2 billion LamportsPerSol

--- index.js
+++ index.js
@@ -19,8 +19,20 @@ const getWalletBalance = async () => {
     console.error(err);
   }
 };
+const airDropSol = async ()=>{
+  try{
+     const connection = new Connection(clusterApiUrl('devnet'),"confirmed");
+     const fromAirDropSignature = await connection.requestAirdrop(publicKey,2*LAMPORTS_PER_SOL);
+     await connection.confirmTransaction(fromAirDropSignature);
+  }
+  catch(err){
+    console.error(err);
+  }
+}
 
 const main = async () => {
+  await getWalletBalance();
+  await airDropSol();
   await getWalletBalance();
 };
 main();
